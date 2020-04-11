660637c5db4cccd2a3aaef64614eabb9c8d5a968
ismail-spec <61207438+ismail-spec@users.noreply.github.com>
2020-04-12 07:11:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(){
   int players;
   cin >>players;
+  string * player_name = new int [players];
+  for (int i = 0; i < players; ++i)
+    cin >> player_name[i];
 }
