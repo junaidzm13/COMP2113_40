0d6db0aef1fa06152bce609a0b2a607981777cd7
ismail-spec <61207438+ismail-spec@users.noreply.github.com>
2020-04-20 18:34:15 +0800

    Update find_where.cpp

--- find_where.cpp
+++ find_where.cpp
@@ -1,14 +1,24 @@
-void linear_search(string board [][15], string target, int indexes[]){
-  for(int i = 0; i < 15; ++i){
-    for(int j = 0; j < 15; ++j){
-      if (board[i][j] == target) {
-        indexes[0] = i;
-        indexes[1] = j;
-        return;
+void linear_search(int position[], string board [][15], string token ){
+  string word;
+  for (int i=0; i<15; ++i){
+    for (int j=0; j<15;++j){
+      word=board[i][j];
+      if (word.length() <= 2){
+        if (word[0] == token[0] && word[1]==token[1]) {
+          position[0]=i;
+          position[1]=j;
+          return;
+        }
+        else{
+          for (int k=0; k< word.length()/2; ++k){
+            if (word[2*k] == token[0] && word[2*k+1]==token[1]) {
+              position[0]=i;
+              position[1]=j;
+              return;
+            }
+          }
+        }
       }
     }
   }
-  indexes[0] = -1;
-  indexes[1] = -1;
-  return;
 }
