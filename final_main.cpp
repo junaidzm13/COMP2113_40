3dd69481d55793bd742d4a65a6e0e8c001c0398b
ismail-spec <61207438+ismail-spec@users.noreply.github.com>
2020-05-01 06:04:40 +0800

    Update final_main.cpp

--- final_main.cpp
+++ final_main.cpp
@@ -422,10 +422,166 @@ void set_the_board(Player_current_details *players, int num_players, string boar
   }
 }
 
+void token_movement(int player_number, int dice_roll, int &row_index, int &col_index){
+  int move1, move2, move3;
+  if(row_index >= 6 && row_index <= 8){ //near player 1's home
+    if(col_index < 6 && row_index == 6){
+      move1 = min(5 - col_index, dice_roll);
+      move2 = max(dice_roll - move1, 0);
+      col_index = col_index + move1 + (move2 > 0);
+      row_index = row_index - move2;
+    }
+    else if(col_index < 6 && row_index == 8){ //near to player1's home
+      if(player_number == 1){
+        move1 = min(col_index, dice_roll);
+        move2 = min(dice_roll - move1, 1);
+        move3 = dice_roll - move1 - move2;
+        col_index = col_index - move1 + move3;
+        row_index = row_index - move2;
+      }
+      else{
+        move1 = min(col_index, dice_roll);
+        move2 = min(dice_roll - move1, 2);
+        move3 = dice_roll - move1 - move2;
+        col_index = col_index - move1 + move3;
+        row_index = row_index - move2;
+      }
+    }
+    else if(col_index < 6 && row_index == 7){
+      if(player_number == 1){
+        row_index = row_index;
+        if(dice_roll > 6 - col_index){
+          col_index = col_index;
+        }
+        else{
+          col_index = col_index + dice_roll;
+        }
+      }
+      else{
+        row_index = row_index - 1;
+        col_index = col_index + dice_roll - 1;
+      }
+    }
+    else if(col_index > 8 && row_index == 6){//near player 3's home
+      if(player_number == 2){
+        move1 = min(14 - col_index, dice_roll);
+        move2 = min(dice_roll - move1, 1);
+        move3 = dice_roll - move1 - move2;
+        col_index = col_index + move1 - move3;
+        row_index = row_index + move2;
+      }
+      else{
+        move1 = min(14 - col_index, dice_roll);
+        move2 = min(dice_roll - move1, 2);
+        move3 = dice_roll - move1 - move2;
+        col_index = col_index + move1 - move3;
+        row_index = row_index + move2;
+      }
+    }
+    else if(col_index > 8 && row_index == 8){
+      move1 = min(col_index - 9, dice_roll);
+      move2 = max(dice_roll - move1, 0);
+      col_index = col_index - move1 - (move2 > 0);
+      row_index = row_index + move2;
+    }
+    else if(col_index > 8 && row_index == 7){
+      if(player_number == 2){
+        row_index = row_index;
+        if(dice_roll > col_index - 8){
+          col_index = col_index;
+        }
+        else{
+          col_index = col_index - dice_roll;
+        }
+      }
+      else{
+        row_index = row_index + 1;
+        col_index = col_index - dice_roll + 1;
+      }
+    }
+  }
+  else if(col_index >= 6 && col_index <= 8){
+    if(row_index < 6 && col_index == 8){ //near player2's home
+      move1 = min(5 - row_index, dice_roll);
+      move2 = max(dice_roll - move1, 0);
+      row_index = row_index + move1 + (move2 > 0);
+      col_index = col_index + move2;
+    }
+    else if(row_index < 6 && col_index == 6){
+      if(player_number == 4){
+        move1 = min(row_index, dice_roll);
+        move2 = min(dice_roll - move1, 1);
+        move3 = dice_roll - move1 - move2;
+        row_index = row_index - move1 + move3;
+        col_index = col_index + move2;
+      }
+      else{
+        move1 = min(row_index, dice_roll);
+        move2 = min(dice_roll - move1, 2);
+        move3 = dice_roll - move1 - move2;
+        row_index = row_index - move1 + move3;
+        col_index = col_index + move2;
+      }
+    }
+    else if(row_index < 6 && col_index == 7){
+      if(player_number == 4){
+        col_index = col_index;
+        if(dice_roll > 6 - row_index){
+          row_index = row_index;
+        }
+        else{
+          row_index = row_index + dice_roll;
+        }
+      }
+      else{
+        col_index = col_index + 1;
+        row_index = row_index + dice_roll - 1;
+      }
+    }
+    else if(row_index > 8 && col_index == 8){
+      if(player_number == 3){
+        move1 = min(14 - row_index, dice_roll);
+        move2 = min(dice_roll - move1, 1);
+        move3 = dice_roll - move1 - move2;
+        row_index = row_index + move1 - move3;
+        col_index = col_index - move2;
+      }
+      else{
+        move1 = min(14 - row_index, dice_roll);
+        move2 = min(dice_roll - move1, 2);
+        move3 = dice_roll - move1 - move2;
+        row_index = row_index + move1 - move3;
+        col_index = col_index - move2;
+      }
+    }
+    else if(row_index > 8 && col_index == 6){
+      move1 = min(row_index - 9, dice_roll);
+      move2 = max(dice_roll - move1, 0);
+      row_index = row_index - move1 - (move2 > 0);
+      col_index = col_index - move2;
+    }
+    else if(row_index > 8 && col_index == 7){
+      if(player_number == 3){
+        col_index = col_index;
+        if(dice_roll > row_index - 8){
+          row_index = row_index;
+        }
+        else{
+          row_index = row_index - dice_roll;
+        }
+      }
+      else{
+        col_index = col_index - 1;
+        row_index = row_index - dice_roll + 1;
+      }
+    }
+  }
+}
+
 
 bool can_move(string board[][15], int player_number, int dice_roll, int row_index, int col_index){
   bool can_be_moved;
-  if((col_index < 6 && (row_index < 6 || row_index > 8)) || \ //in base or not
+  if((col_index < 6 && (row_index < 6 || row_index > 8)) || \ 
   (col_index > 8 && (row_index < 6 || row_index > 8))){
     if(dice_roll == 6){
       return 1;
@@ -434,8 +590,7 @@ bool can_move(string board[][15], int player_number, int dice_roll, int row_inde
       return 0;
     }
   }
-  can_be_moved = ! (col_index == 7 && (row_index == 6 || row_index == 8) || \ //has reached home or not
-  row_index == 7 && (col_index == 6 || col_index == 8));
+  can_be_moved = ! (col_index == 7 && (row_index == 6 || row_index == 8) || row_index == 7 && (col_index == 6 || col_index == 8));  //has reached home or not
   int col_index2 = col_index, row_index2 = row_index;
   token_movement(player_number, dice_roll, row_index2, col_index2);
   return !(col_index == col_index2 && row_index == row_index2) && \
@@ -557,6 +712,7 @@ void send_home(string board[][15], int player_killed, string token2){
 }
 
 
+
 void replace_old_position(string board[][15], int row, int column, string token){
   if (board[row][column].length() > 2){   //if double token on previous place, so what will happen to previous location is
     for (int i=0; i< board[row][column].length(); ++i){
@@ -633,9 +789,6 @@ void replace_new_position(Player_current_details *players, int player_number, st
   //}
 }
 
-
-
-
 void make_a_move(Player_current_details *player, int turn_count, int player_number, int No_of_sixes, int dice_number, string board[][15], int &Num_player_won){
   int position [2];
   string token;
@@ -699,6 +852,7 @@ void make_a_move(Player_current_details *player, int turn_count, int player_numb
       if (has_won()){
         Num_player_won += 1;
         player[turn_count].rank = Num_player_won;
+        return;
       }
 
     }
@@ -769,6 +923,6 @@ int main(){
   }
 
   players_current_progress[num_players - 1].rank = num_players -1;
-  
+
 
 }
