b9aae303cc458a32778ca534594f2afdae1b87f2
Muhammad Junaid Zubair Malik <62522218+junaidzm13@users.noreply.github.com>
2020-05-07 06:12:11 +0500

    Add read_username.cpp & read_username.h
    Added finalized versions of read_username.cpp and read_username.h

---
+++ read_username.h
@@ -0,0 +1,12 @@
+//read_username.h
+#include <iostream>
+#include <string>
+using namespace std;
+#ifndef READ_USERNAME_H
+#define READ_USERNAME_H
+
+void create_new(string username);
+bool read_username(string username);
+string enter_username();
+
+#endif
