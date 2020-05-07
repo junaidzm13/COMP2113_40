a02749da0e75642448ceb893f9b793c7dfa31c04
ismail-spec <61207438+ismail-spec@users.noreply.github.com>
2020-05-08 00:04:48 +0800

    Added roll_dice.cpp and roll_dice.h
    Added roll_dice.cpp and roll_dice.h

---
+++ roll_dice.h
@@ -0,0 +1,11 @@
+//roll_dice.h
+#include <iostream>
+#include <string>
+#include "structs.h"
+using namespace std;
+#ifndef ROLL_DICE_H
+#define ROLL_DICE_H
+
+int roll_dice(Player_current_details *player, int turn_count, int &No_of_sixes, int &dice_number);
+
+#endif
