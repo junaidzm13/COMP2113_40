9b7d2d464ef5f660ad5093fe8f75e16c097e4f80
ismail-spec <61207438+ismail-spec@users.noreply.github.com>
2020-05-08 00:05:38 +0800

    Added set_the_board.cpp and set_the_board.h
    Added set_the_board.cpp and set_the_board.h

---
+++ set_the_board.h
@@ -0,0 +1,11 @@
+//set_the_board.h
+#include <iostream>
+#include <string>
+#include "structs.h"
+using namespace std;
+#ifndef SET_THE_BOARD_H
+#define SET_THE_BOARD_H
+
+void set_the_board(Player_current_details players[], int num_players, string board[][15]);
+
+#endif
