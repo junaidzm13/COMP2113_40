1ffc7b4525eec9d04216886071633b606b5f9da3
Muhammad Junaid Zubair Malik <62522218+junaidzm13@users.noreply.github.com>
2020-05-07 06:07:42 +0500

    Add load_profile.cpp & .h
    Added finalized versions of load_profile.cpp nad load_profile.h

---
+++ load_profile.h
@@ -0,0 +1,10 @@
+//load_profile.h
+#include <string>
+#include "structs.h"
+using namespace std;
+#ifndef LOAD_PROFILE_H
+#define LOAD_PROFILE_H
+
+void load_profile(Profile &user, string username);
+
+#endif
