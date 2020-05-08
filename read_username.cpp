8cd25560a5499a456bdb3ac1d2346bbbbe9dead0
Muhammad Junaid Zubair Malik <62522218+junaidzm13@users.noreply.github.com>
2020-05-09 02:21:52 +0500

    Update read_username.cpp
    Added an extra feature ( there can be no duplicate usernames).

--- read_username.cpp
+++ read_username.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <fstream>
 #include "read_username.h"
+#include "structs.h"
 using namespace std;
 
 /*This function takes a string username representing a new username, and creates a text
@@ -24,32 +25,41 @@ void create_new(string username){
   fout.close();
 }
 
-/*This function takes a string username and returns a boolean whether such
+/*This function takes a string username, a pointer to array of Player_current_details players 
+and an integer i representing one less player number, and returns a boolean whether such
 a username exists in the file usernames.txt or not.*/
-bool read_username(string username){
+bool read_username(string username, Player_current_details *players, int i){
   string word;
-  int exists = 0;
+  int isvalid = 0;
   ifstream fin;
   fin.open("usernames.txt");
   while(fin >> word){
     if(word == username){
-      exists = 1;
-      return 1;
+      isvalid = 1;
+    }
+  }
+  if(isvalid == 1){
+    for(int j = 0; j < i; j++){
+      if(username == players[j].username){
+        isvalid = 0;
+      }
     }
   }
   fin.close();
-  return 0;
+  return isvalid;
 }
 
-/*This function takes no parameters. Prompts the user to enter his/her username. Calls
-read_username to check whether such a username exists. If it does, returns that username
-and if it does not, creates a new file <username>.txt and returns the new username entered.*/
-string enter_username(){
+/*This function takes two parameters, a pointer to an array of Player_current_details and an 
+integer i representing player number less one. Prompts the user to enter his/her username. Calls
+read_username to check whether such a username exists and has not been taken already. If it does,
+returns that username and if it does not, creates a new file <username>.txt and returns the 
+new username entered.*/
+string enter_username(Player_current_details *players, int i){
   char createnew;
   string username;
   cout << "Please enter your username: ";
   cin >> username;
-  while(read_username(username) != 1){
+  while(read_username(username, players, i) != 1){
     cout << "No user found with that username.\nDo you want to create a new profile? (Y/N)" << endl;
     cin >> createnew;
     while(createnew != 'Y' && createnew != 'N'){
