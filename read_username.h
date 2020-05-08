f910716bf336cd345663c69193580206bf8b59d3
Muhammad Junaid Zubair Malik <62522218+junaidzm13@users.noreply.github.com>
2020-05-09 02:24:47 +0500

    Update read_username.h
    Added the newly added parameters to the function declarations and structs header file.

--- read_username.h
+++ read_username.h
@@ -1,12 +1,13 @@
 //read_username.h
 #include <iostream>
 #include <string>
+#include "structs.h"
 using namespace std;
 #ifndef READ_USERNAME_H
 #define READ_USERNAME_H
 
 void create_new(string username);
-bool read_username(string username);
-string enter_username();
+bool read_username(string username, Player_current_details *players, int i);
+string enter_username(Player_current_details *players, int i);
 
 #endif
